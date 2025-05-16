a73e3c8b8e0eaa8314e94a70ff1b20272fbcc7b3
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-17 04:16:47 +0900

    [ADD] ExampleService 추가

---
+++ Sopkathon/Sopkathon/Network/Service/ExampleService.swift
@@ -0,0 +1,46 @@
+//
+//  ExampleService.swift
+//  Sopkathon
+//
+//  Created by 권석기 on 5/17/25.
+//
+
+import Foundation
+
+import Moya
+
+/*
+ API를 설정했다면 provider를 생성하고 요청해주면 됩니다.
+ 
+ 1. Provider 생성
+ 
+ 2. async 혹은 클로저로 입맛에 맞게 사용
+ 
+ 3. 리턴값 유무
+ 
+ 단순히 요청만 필요한 경우
+ ```
+ try awwait provider.async.request.requestPlain(API)
+ ```
+ 
+ 리턴값이 필요한 경우(제네릭)
+ ```
+ let result: T = try await provier.async.request(API)
+ ```
+ */
+
+final class ExampleService {
+    
+    static let shared = ExampleModel()
+    
+    // Provider에 사용하려는 API를 제네릭으로 넘겨주세요
+    let provider = MoyaProvider<ExampleAPI>()
+    
+    func exmapleRequest() async {
+        do {
+            try await provider.async.requestPlain(.exampleRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
