ec6b5cc7059756c05de76324490392e29b83bc6d
sem-git <lsmsm0327@naver.com>
2025-05-18 04:29:16 +0900

    [FEAT] MypageViewController 내가 참여한 활동 제외한 UI 구현

--- Sopkathon/Sopkathon/Presentation/Mypage/MypageViewController.swift
+++ Sopkathon/Sopkathon/Presentation/Mypage/MypageViewController.swift
@@ -6,17 +6,18 @@
 //
 
 import UIKit
-
 import SnapKit
 import Then
 
 final class MypageViewController: UIViewController {
     
+    private let topBackgroundView = UIView()
+    private let profileBackgroundView = UIView()
     private let profileImageView = UIImageView()
     private let nameLabel = UILabel()
     private let phoneNumberLabel = UILabel()
-    
     private var myActivityLabel = UILabel()
+    private var suggestLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +25,25 @@ final class MypageViewController: UIViewController {
         setStyle()
         setUI()
         setLayout()
-        fetchMypageData()
+        // fetchMypageData()
     }
     
     private func setStyle() {
         self.view.backgroundColor = .white
         
+        topBackgroundView.backgroundColor = UIColor(red: 0.971, green: 1, blue: 0.984, alpha: 1)
+        
+        profileBackgroundView.do {
+            $0.backgroundColor = UIColor(red: 0.964, green: 0.964, blue: 0.964, alpha: 1)
+            $0.layer.cornerRadius = 45.5
+            $0.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1).cgColor
+            $0.layer.borderWidth = 1
+            $0.clipsToBounds = true
+        }
+        
         profileImageView.do {
-            $0.image = UIImage(systemName: "person.circle")
-            $0.layer.masksToBounds = true
-            $0.layer.cornerRadius = $0.frame.size.width / 2
+            $0.image = UIImage(resource: .icMypage)
+            $0.contentMode = .scaleAspectFit
         }
         
         nameLabel.do {
@@ -49,42 +59,64 @@ final class MypageViewController: UIViewController {
         }
         
         myActivityLabel.do {
-            $0.text = "참여한 활동"
+            $0.text = "내가 참여한 활동"
             $0.textColor = .black
-            $0.font = .systemFont(ofSize: 25)
+            $0.font = .systemFont(ofSize: 20)
         }
         
+        suggestLabel.do {
+            $0.text = "후기를 작성해보세요!"
+            $0.textColor = UIColor(red: 0.499, green: 0.499, blue: 0.499, alpha: 1)
+            $0.font = .systemFont(ofSize: 12)
+        }
     }
     
     private func setUI() {
-        view.addSubviews(
-            profileImageView, nameLabel, phoneNumberLabel, myActivityLabel
-        )
+        view.addSubviews(topBackgroundView, myActivityLabel, suggestLabel)
+        topBackgroundView.addSubviews(profileBackgroundView, nameLabel, phoneNumberLabel)
+        profileBackgroundView.addSubview(profileImageView)
     }
     
     private func setLayout() {
-        profileImageView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+        topBackgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(phoneNumberLabel.snp.bottom).offset(38)
+        }
+        
+        profileBackgroundView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(55)
             $0.centerX.equalToSuperview()
-            $0.size.equalTo(80)
+            $0.size.equalTo(91)
+        }
+        
+        profileImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(48)
         }
         
         nameLabel.snp.makeConstraints {
-            $0.top.equalTo(profileImageView.snp.bottom).offset(10)
+            $0.top.equalTo(profileBackgroundView.snp.bottom).offset(20)
             $0.centerX.equalToSuperview()
         }
         
         phoneNumberLabel.snp.makeConstraints {
-            $0.top.equalTo(nameLabel.snp.bottom).offset(10)
+            $0.top.equalTo(nameLabel.snp.bottom).offset(8)
             $0.centerX.equalToSuperview()
         }
         
         myActivityLabel.snp.makeConstraints {
-            $0.top.equalTo(phoneNumberLabel.snp.bottom).offset(60)
-            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(topBackgroundView.snp.bottom).offset(24)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        suggestLabel.snp.makeConstraints {
+            $0.top.equalTo(myActivityLabel.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().offset(16)
         }
     }
     
+    /*
     private func fetchMypageData() {
         let userId = 5
         
@@ -102,4 +134,5 @@ final class MypageViewController: UIViewController {
             }
         }
     }
+    */
 }
