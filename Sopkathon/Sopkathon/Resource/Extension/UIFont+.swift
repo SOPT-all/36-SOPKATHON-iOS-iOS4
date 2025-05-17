acf39a75ab978c581bb164f62e1609ceb7a44e20
yeonee <leenayeon@sookmyung.ac.kr>
2025-05-18 03:56:43 +0900

    [ADD] 폰트 디자인시스템 추가

---
+++ Sopkathon/Sopkathon/Resource/Extension/UIFont+.swift
@@ -0,0 +1,46 @@
+//
+//  UIFont+.swift
+//  Sopkathon
+//
+//  Created by 이나연 on 5/18/25.
+//
+
+import UIKit
+
+extension UIFont {
+    class var head_sb_20: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 20)!
+    }
+    
+    class var head_sb_16: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 16)!
+    }
+    
+    class var head_rg_16: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16)!
+    }
+    
+    class var head_sb_14: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 14)!
+    }
+    
+    class var body_rg_16: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16)!
+    }
+    
+    class var body_md_14: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 14)!
+    }
+    
+    class var body_rg_12: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 12)!
+    }
+    
+    class var body_rg_14: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14)!
+    }
+    
+    class var cap_md_20: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 20)!
+    }
+}
