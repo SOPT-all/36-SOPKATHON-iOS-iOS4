5bb777ff1e86e98dfed9ef564aa6862a0edd1385
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-18 04:16:16 +0900

    [FEAT] TabBar Border 수정

--- Sopkathon/Sopkathon/Presentation/MainTab/CustomTapBarViewController.swift
+++ Sopkathon/Sopkathon/Presentation/MainTab/CustomTapBarViewController.swift
@@ -35,6 +35,9 @@ final class CustomTabBarVC: UITabBarController {
         myPageItem.image = UIImage(resource: .myPageIcon)
         homeItem.imageInsets = .init(top: 35, left: 0, bottom: 0, right: 0)
         myPageItem.imageInsets = .init(top: 35, left: 0, bottom: 0, right: 0)
+        tabBar.layer.borderWidth = 0.50
+        tabBar.layer.borderColor = UIColor.gray.cgColor
+        tabBar.clipsToBounds = true
     }
 }
 
