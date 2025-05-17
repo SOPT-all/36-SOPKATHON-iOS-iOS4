cd3467aaefaebb9dae664d6f359bec96bdd1147e
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-18 04:13:58 +0900

    [FIX] TabBar 간격 조정

--- Sopkathon/Sopkathon/Presentation/MainTab/CustomTapBarViewController.swift
+++ Sopkathon/Sopkathon/Presentation/MainTab/CustomTapBarViewController.swift
@@ -24,12 +24,17 @@ final class CustomTabBarVC: UITabBarController {
         
         self.setViewControllers([homeVC, reviewVC], animated: false)
         self.modalPresentationStyle = .fullScreen
-        // TODO: 여기 gray를 Main으로 바꿔주세요
-        self.tabBar.backgroundColor = .gray
+        // TODO: 여기 purple를 Main으로 바꿔주세요
+        self.tabBar.tintColor = .purple
+        self.tabBar.backgroundColor = .white
         
         guard let items = self.tabBar.items else { return }
-        items[0].image = UIImage(resource: .homeIcon)
-        items[1].image = UIImage(resource: .myPageIcon)
+        let homeItem = items[0]
+        let myPageItem = items[1]
+        homeItem.image = UIImage(resource: .homeIcon)
+        myPageItem.image = UIImage(resource: .myPageIcon)
+        homeItem.imageInsets = .init(top: 35, left: 0, bottom: 0, right: 0)
+        myPageItem.imageInsets = .init(top: 35, left: 0, bottom: 0, right: 0)
     }
 }
 
